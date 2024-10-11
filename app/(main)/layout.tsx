f94bdb0d65ec3aadc11308defcb8d774b8a21b5f
Daffa Alif Murtaja <daffa4lif@gmail.com>
2024-10-11 22:22:53 +0700

    impelement sidebar

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+import Sidebar from '@/components/sidebar'
 import React from 'react'
 
 type Props = { children: React.ReactNode }
@@ -5,9 +6,8 @@ type Props = { children: React.ReactNode }
 const Layout = (props: Props) => {
   return (
     <div className='flex overflow-hidden h-screen'>
-        <div className="w-full">
-            {props.children}
-        </div>
+      <Sidebar />
+      <div className="w-full">{props.children}</div>
     </div>
   )
 }
