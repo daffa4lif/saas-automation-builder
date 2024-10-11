c2e04f99e49f53fde0923d30bbb9ee176f297263
Daffa Alif Murtaja <daffa4lif@gmail.com>
2024-10-11 10:58:10 +0700

    fix: center item

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex items-center justify-center flex-col">
       <Navbar />
       <section className="h-screen w-full bg-neutral-950 rounded-md !overflow-visible relative flex flex-col items-center antialiased">
         {/* <div className="max-w-4xl mx-auto p-4 relative z-10 flex items-center justify-center flex-col"></div> */}
